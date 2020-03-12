e073a4a354c37369bfcd020d6470ca75fb483712
dodadoa <maddodadoa@gmail.com>
2020-03-12 11:43:07 +0700

    :sparkles: add rotating header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import IndexStyles from "./index.module.css"
 
 const IndexPage = () => (
-  <Layout>
+  <Layout className={IndexStyles}>
     <SEO title="Home" />
-    <h1> TART </h1>
-    <p>
+    <h1> T / ART </h1>
+    <div className={IndexStyles.contentContainer}>
+      <svg 
+        viewBox="0 0 100 100" 
+        width="100" 
+        height="100"
+        className={IndexStyles.circleCanvas}>
+        <defs>
+          <path 
+            id="circle"
+            d="M 50, 50
+              m -37, 0
+              a 37,37 0 1,1 74,0
+              a 37,37 0 1,1 -74,0"
+          />
+        </defs>
+        <text font-size="17">
+          <textPath xlink= {true} href="#circle">
+            what &nbsp;&nbsp; is &nbsp;&nbsp; t/art
+          </textPath>
+        </text>
+      </svg>
+    </div>
+    
+    <p className={IndexStyles.content}>
       {`
         A platform for developers and artist to develop a project based on mutual 
         interest and collaboration furthering the discourse of technology.  
